08bc54d6f76dc9b99c7ce2f1168f690406171917
anandj1 <anandj12215@gmail.com>
2024-07-25 21:36:06 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color,setColor] = useState('yellow')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className=' w-full h-screen' style={{backgroundColor:color}}>
+    <div className=' flex justify-center '>
+    <div className=' flex justify-center items-center gap-7 p-4 mt-4 rounded-lg bg-yellow-500 ' >
+    <button className=' rounded-md bg-red-600 text-black p-5 font-bold text-2xl ' onClick={()=>{
+      setColor('Red')
+    }}>Red</button>
+    
+     <button className=' rounded-md bg-green-600 text-black p-5 font-bold text-2xl' onClick={()=>{
+      setColor('green')}}>Green</button>
+     <button className=' rounded-md bg-blue-600 text-black p-5 font-bold text-2xl'onClick={()=>{
+      setColor('blue')}} >Blue</button>
+    </div>
+ 
+      
+
+
+
+    </div>
+
+
+
+    </div>
+    
+  
     </div>
   );
 }
